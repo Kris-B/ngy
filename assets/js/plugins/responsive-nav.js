265f8d4aa6566f6e63bc2e9ca831c95982724d5e
Christophe Brisbois <chr@brisbois.fr>
2016-03-18 16:30:42 +0100

    Update responsive-nav.js

--- assets/js/plugins/responsive-nav.js
+++ assets/js/plugins/responsive-nav.js
@@ -126,9 +126,9 @@ var responsiveNav = (function (window, document) {
 
       // Default options
       this.options = {
-        animate: true,        // Boolean: Use CSS3 transitions, true or false
-        transition: 400,      // Integer: Speed of the transition, in milliseconds
-        label: "Menu",        // String: Label for the navigation toggle
+        animate: false,        // Boolean: Use CSS3 transitions, true or false
+        transition: 200,      // Integer: Speed of the transition, in milliseconds
+        label: "menu",        // String: Label for the navigation toggle
         insert: "after",      // String: Insert the toggle before or after the navigation
         customToggle: "",     // Selector: Specify the ID of a custom toggle
         openPos: "relative",  // String: Position of the opened nav, relative or static
